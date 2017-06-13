a04a7c4d6c34b2a82cac3384dd2f07a851e60ca7
Aye Mya Thu <ayemyathu@Ayes-MacBook-Air.local>
2017-06-13 10:38:22 +0900

    first upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,22 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import * as firebase from 'firebase';
+
+//<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+//<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSkt0IowehBQiSv6In1HqJh8mYRSr1p9c",
+    authDomain: "ionicchatapp-6da25.firebaseapp.com",
+    databaseURL: "https://ionicchatapp-6da25.firebaseio.com",
+    projectId: "ionicchatapp-6da25",
+    storageBucket: "ionicchatapp-6da25.appspot.com",
+    messagingSenderId: "147852898346"
+  };
+  firebase.initializeApp(config);
+//</script>
+
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
